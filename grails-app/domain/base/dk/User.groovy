c47a8f028cd942df3e088d60fc7718f27de0ddfe
dekay2323 <dekay.2323@gmail.com>
2017-07-03 12:57:07 -0600

    Checked in wrong User

--- grails-app/domain/base/dk/User.groovy
+++ grails-app/domain/base/dk/User.groovy
@@ -1,4 +1,4 @@
-package com.pro3.domain.user
+package base.dk
 
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
@@ -12,31 +12,14 @@ class User implements Serializable {
 	transient springSecurityService
 
 	String username
-	String email
 	String password
 	boolean enabled = true
 	boolean accountExpired
 	boolean accountLocked
 	boolean passwordExpired
-	Account account
-
-	// @TODO should be able to handle several accounts
-	Account obtainAccount() {
-		Account.createCriteria().list{
-			users{
-				eq('id', id)
-			}
-		}[0]
-	}
-
-
 
 	Set<Role> getAuthorities() {
 		UserRole.findAllByUser(this)*.role
-	}
-
-	List<String> getAuthorityNames() {
-		getAuthorities()*.name
 	}
 
 	def beforeInsert() {
@@ -56,18 +39,11 @@ class User implements Serializable {
 	static transients = ['springSecurityService']
 
 	static constraints = {
-		password nullable: false, blank: false, password: true
-		username nullable: false, blank: false, unique: true
-		email nullable: false, blank: false, email: true
-		account nullable: true
+		password blank: false, password: true
+		username blank: false, unique: true
 	}
 
 	static mapping = {
 		password column: '`password`'
-		sort username: 'asc'
-	}
-
-	public String toString() {
-		"${username}"
 	}
 }
